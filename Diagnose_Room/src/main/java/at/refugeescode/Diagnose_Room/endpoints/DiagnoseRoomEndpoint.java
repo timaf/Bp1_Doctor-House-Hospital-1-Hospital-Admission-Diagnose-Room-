26c48ab08a4c59e925333a0f20b77b5d909ef630
timaf <lo_sa_na@yahoo.com>
2018-04-01 20:28:38 +0200

    starting

--- Diagnose_Room/src/main/java/at/refugeescode/Diagnose_Room/endpoints/DiagnoseRoomEndpoint.java
+++ Diagnose_Room/src/main/java/at/refugeescode/Diagnose_Room/endpoints/DiagnoseRoomEndpoint.java
@@ -1,28 +1,37 @@
 package at.refugeescode.Diagnose_Room.endpoints;
 
+import at.refugeescode.Diagnose_Room.logic.DrHouseDiagnose;
 import at.refugeescode.Diagnose_Room.model.Patient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/patients")
 public class DiagnoseRoomEndpoint {
 
     private RestTemplate restTemplate;
+    private Patient patient;
+    private DrHouseDiagnose drHouseDiagnose;
+    private List<Patient> patients = new ArrayList <>();
 
-    public DiagnoseRoomEndpoint(RestTemplate restTemplate) {
+    public DiagnoseRoomEndpoint(RestTemplate restTemplate, Patient patient) {
         this.restTemplate = restTemplate;
     }
 
     @GetMapping
-    String sayHello(){
-        return "HHHH";
+    List<Patient> receivePatient(){
+        return patients;
     }
     @PostMapping
-    Patient lookAt(@RequestBody Patient patient){
-        return patient;
-
+    Patient addPatient(@RequestBody Patient patient){
+        patients.add(patient);
+        Patient patient1 = drHouseDiagnose.diagnosePatient(patient);
+        String nurseryUrl = "http://localhost:patients";
+        Patient patient2 = restTemplate.postForObject(nurseryUrl, patient1, Patient.class);
+        return patient2;
     }
 
 
--- Diagnose_Room/src/main/java/at/refugeescode/Diagnose_Room/logic/DrHouseDiagnose.java
+++ Diagnose_Room/src/main/java/at/refugeescode/Diagnose_Room/logic/DrHouseDiagnose.java
@@ -1,4 +1,30 @@
 package at.refugeescode.Diagnose_Room.logic;
 
+import at.refugeescode.Diagnose_Room.model.Patient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class DrHouseDiagnose {
+
+    private Patient patient;
+
+    @Value("#{'${illnesses}'.split(',')}")
+    private List<String> illnesses ;
+
+  public Patient  diagnosePatient(Patient patient){
+      if(patient.getSymptoms().contains("fever"))
+          patient.setIllness(illnesses.get(0));
+      else if (patient.getSymptoms().contains("dizziness"))
+          patient.setIllness(illnesses.get(1));
+      else if (patient.getSymptoms().contains("pain"))
+          patient.setIllness(illnesses.get(2));
+      else if (patient.getSymptoms().contains("dizziness"))
+          patient.setIllness(illnesses.get(3));
+      else
+          patient.setIllness("Lupus");
+      return patient;
+  }
 }
