dde3becb992aa2251615aa7e8ec080557a7d1079
timaf <lo_sa_na@yahoo.com>
2018-04-01 20:53:14 +0200

    start

---
+++ Diagnose_Room/src/main/java/at/refugeescode/Diagnose_Room/DiagnoseRoomApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.Diagnose_Room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnoseRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagnoseRoomApplication.class, args);
+	}
+}
---
+++ Hospital_Admission/src/main/java/at/refugeescode/Hospital_Admission/HospitalAdmissionApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.Hospital_Admission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalAdmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalAdmissionApplication.class, args);
+	}
+}
